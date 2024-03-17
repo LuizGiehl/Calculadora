71dade0c93942fc3e1d77e219809e26ff9db3908
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2024-03-17 18:56:11 -0300

    funcionalidade nos botões de operação

--- src/script.js
+++ src/script.js
@@ -1,8 +1,54 @@
 function insert(num) {
     var numero = document.getElementById('resultado').innerHTML;
+
+
+
+
+    
     document.getElementById('resultado').innerHTML = numero + num;
+    const conta = document.getElementById('resultado').innerHTML;
+    const ultNum = conta.slice(-1);
+    
+    var menos = document.getElementById('menos');
+    var mais = document.getElementById('mais');
+    var vezes = document.getElementById('vezes');
+    var divi = document.getElementById('divi');
+
+    if(ultNum === '/' || ultNum === '*' || ultNum === '-' || ultNum === '+') {
+
+        divi.removeAttribute('onclick');
+        mais.removeAttribute('onclick');
+        menos.removeAttribute('onclick');
+        vezes.removeAttribute('onclick');
+    }
+    else if(ultNum != '/' && ultNum != '*' && ultNum != '-' && ultNum != '+'){
+        divi.setAttribute('onclick', "insert('/')");
+        mais.setAttribute('onclick', "insert('+')");
+        menos.setAttribute('onclick', "insert('-')");
+        vezes.setAttribute('onclick', "insert('*')");
+
+    };
+
+    console.log(divi);
+    console.log(vezes);
+    console.log(mais);
+    console.log(menos);
+
+
 }
 
 function clean() {
-    document.getElementById('resultado').innerHTML = ''
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function back() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calc() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
 }
