9a6954abdfb3ec691281d2eb2ef572b1945969a9
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2024-03-20 16:03:41 -0300

    calculadora finalizada

--- src/script.js
+++ src/script.js
@@ -1,9 +1,5 @@
 function insert(num) {
     var numero = document.getElementById('resultado').innerHTML;
-
-
-
-
     
     document.getElementById('resultado').innerHTML = numero + num;
     const conta = document.getElementById('resultado').innerHTML;
@@ -13,42 +9,56 @@ function insert(num) {
     var mais = document.getElementById('mais');
     var vezes = document.getElementById('vezes');
     var divi = document.getElementById('divi');
-
-    if(ultNum === '/' || ultNum === '*' || ultNum === '-' || ultNum === '+') {
+    var ponto = document.getElementById('ponto');
+    
+    if(ultNum === '/' || ultNum === '*' || ultNum === '-' || ultNum === '+' || ultNum === '.') {
 
         divi.removeAttribute('onclick');
         mais.removeAttribute('onclick');
         menos.removeAttribute('onclick');
         vezes.removeAttribute('onclick');
+        ponto.removeAttribute('onclick');
     }
-    else if(ultNum != '/' && ultNum != '*' && ultNum != '-' && ultNum != '+'){
+    else if(ultNum != '/' && ultNum != '*' && ultNum != '-' && ultNum != '+' && ultNum != '.'){
         divi.setAttribute('onclick', "insert('/')");
         mais.setAttribute('onclick', "insert('+')");
         menos.setAttribute('onclick', "insert('-')");
         vezes.setAttribute('onclick', "insert('*')");
-
+        ponto.setAttribute('onclick', "insert('.')")
+        
     };
-
+    
     console.log(divi);
     console.log(vezes);
     console.log(mais);
     console.log(menos);
-
-
-}
-
-function clean() {
-    document.getElementById('resultado').innerHTML = '';
-}
+    console.log(ultNum)
+    
+};
 
 function back() {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
-}
+    divi.setAttribute('onclick', "insert('/')");
+    mais.setAttribute('onclick', "insert('+')");
+    menos.setAttribute('onclick', "insert('-')");
+    vezes.setAttribute('onclick', "insert('*')");
+    ponto.setAttribute('onclick', "insert('.')")
+    
+};
+
+function clean() {
+    document.getElementById('resultado').innerHTML = '';
+};
+
 
 function calc() {
     var resultado = document.getElementById('resultado').innerHTML;
+
     if(resultado) {
-        document.getElementById('resultado').innerHTML = eval(resultado)
+        document.getElementById('resultado').innerHTML = eval(resultado);
     }
-}
+    if(resultado  === '') {
+        window.alert("VERIFIQUE SE SEUS NUMEROS ESTÃO CORRETOS");
+    }
+};
